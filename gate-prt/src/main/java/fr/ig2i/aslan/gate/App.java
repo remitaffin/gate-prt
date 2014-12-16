869bbefc254a087951d4c1ce1dc05196b11be834
KBah <bahesperance@gmail.com>
2014-12-16 19:02:28 +0100

    End of initialisation

--- gate-prt/src/main/java/fr/ig2i/aslan/gate/App.java
+++ gate-prt/src/main/java/fr/ig2i/aslan/gate/App.java
@@ -1,29 +1,14 @@
 package fr.ig2i.aslan.gate;
 
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Logger;
-
-import fr.ig2i.aslan.gate.gazetteer.Gazetteer;
-import fr.ig2i.aslan.gate.sentence_splitter.SentenceSplitter;
-import gate.Gate;
 import gate.util.GateException;
 
-
 public class App {
-	private static Logger LOGGER = Logger.getRootLogger();
-	static SentenceSplitter s = new SentenceSplitter();
-	static Gazetteer g = new Gazetteer();
-	
+
 	public static void main(String[] args) throws GateException {
-		//Set log4j.properties path
-		BasicConfigurator.configure();
-		if (!Gate.isInitialised()) {
-			LOGGER.info("Initialising GATE...");
-			//Set Gate Home
-			System.setProperty("gate.home", "..");
-			//Initialize Gate
-			Gate.init();
-			LOGGER.info("...GATE initialised");
-		}
+		
+		/* 1. initialisze GATE library */
+		InitApp.initGate();
+		/* 2. load Annie plugin */
+		InitApp.loadAnnie();
 	}
 }
