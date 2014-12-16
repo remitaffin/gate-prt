877039a3a22766cb5f2367b2a7983c4fdc83ad0a
Ugo Marinelli <Ugo@MacBook-Pro-de-Ugo.local>
2014-12-16 16:40:50 +0100

    parameter App.java

--- gate-prt/src/main/java/fr/ig2i/aslan/gate/App.java
+++ gate-prt/src/main/java/fr/ig2i/aslan/gate/App.java
@@ -1,9 +1,21 @@
 package fr.ig2i.aslan.gate;
 
+import org.apache.log4j.BasicConfigurator;
+
+import fr.ig2i.aslan.gate.gazetteer.Gazetteer;
+import fr.ig2i.aslan.gate.sentence_splitter.SentenceSplitter;
+import gate.Gate;
 import gate.util.GateException;
 
 public class App {
+	static SentenceSplitter s = new SentenceSplitter();
+	static Gazetteer g = new Gazetteer();
 	public static void main(String[] args) throws GateException {
-		System.out.println("Hello World!");
+		//Set log4j.properties path
+		BasicConfigurator.configure();
+		//Set Gate Home
+		System.setProperty("gate.home", "..");
+		//Initialize Gate
+		Gate.init();	
 	}
 }
