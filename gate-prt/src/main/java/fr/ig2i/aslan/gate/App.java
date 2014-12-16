db93fd7237f40b33dbe14555252db47ee53af74f
KBah <bahesperance@gmail.com>
2014-12-16 16:23:16 +0100

    log configuration + add jar in local lib

--- gate-prt/src/main/java/fr/ig2i/aslan/gate/App.java
+++ gate-prt/src/main/java/fr/ig2i/aslan/gate/App.java
@@ -1,9 +1,20 @@
 package fr.ig2i.aslan.gate;
 
+import gate.Gate;
 import gate.util.GateException;
 
+import org.apache.log4j.Logger;
+
 public class App {
+
+	private static Logger LOGGER = Logger.getRootLogger();
+
 	public static void main(String[] args) throws GateException {
-		System.out.println("Hello World!");
+		if (!Gate.isInitialised()) {
+			LOGGER.info("Initialising GATE...");
+
+			LOGGER.info("...GATE initialised");
+		}
 	}
+
 }
