5919f4e9b56c0e43130ddb19615840e2ed9f607f
Ugo Marinelli <Ugo@MacBook-Pro-de-Ugo.local>
2014-12-16 16:36:12 +0100

    "set init params + added gazetteer class"

---
+++ gate-prt/src/main/java/fr/ig2i/aslan/gate/gazetteer/Gazetteer.java
@@ -0,0 +1,15 @@
+package fr.ig2i.aslan.gate.gazetteer;
+
+import org.apache.log4j.BasicConfigurator;
+
+import gate.Gate;
+import gate.util.GateException;
+
+public class Gazetteer {
+	private static final long serialVersionUID = 1L;
+	public  Gazetteer(){
+
+
+	}
+
+}
