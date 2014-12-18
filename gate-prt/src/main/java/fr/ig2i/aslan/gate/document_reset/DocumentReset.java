6b693c1c7f0abefc602d9f7ff4d627bcef53dd97
bilal <bilal.achetouane@gmail.com>
2014-12-18 18:57:47 +0100

    added document reset

---
+++ gate-prt/src/main/java/fr/ig2i/aslan/gate/document_reset/DocumentReset.java
@@ -0,0 +1,15 @@
+package fr.ig2i.aslan.gate.document_reset;
+
+import gate.creole.annotdelete.AnnotationDeletePR;
+
+
+public class DocumentReset extends AnnotationDeletePR {
+
+	private static final long serialVersionUID = 1L;
+
+	public DocumentReset() {
+		super();
+	}
+	
+
+}
