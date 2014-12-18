2cb3967220020e7f84a8ecabb333d5dc63a57e74
bilal <bilal.achetouane@gmail.com>
2014-12-18 14:57:51 +0100

    added Tokenizer

---
+++ gate-prt/src/main/java/fr/ig2i/aslan/gate/tokenizer/Tokenizer.java
@@ -0,0 +1,11 @@
+package fr.ig2i.aslan.gate.tokenizer;
+
+import gate.creole.tokeniser.SimpleTokeniser;
+
+public class Tokenizer extends SimpleTokeniser {
+	private static final long serialVersionUID = 1L;
+
+	public Tokenizer() {
+		super();
+	}
+}
